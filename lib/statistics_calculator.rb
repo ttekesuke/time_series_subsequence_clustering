6a34c70f468b7df5b5f067a2280581bbe997056c
ttekesuke <tekesuke1986@hotmail.com>
2023-08-31 02:47:12 +0900

    add autocorrelation_coefficient

--- lib/statistics_calculator.rb
+++ lib/statistics_calculator.rb
@@ -1,18 +1,30 @@
 class StatisticsCalculator
+  require 'bigdecimal'
+  require 'bigdecimal/util'
   def mean(d)
-    d.sum / d.length
+    d.sum / d.length.to_d
   end
 
   def covariance(d1, d2)
     raise "The two data lengths are different." if d1.size != d2.size
-    (d1.zip(d2).map{|_d1, _d2 |_d1 * _d2}.sum / d1.size) - (mean(d1) * mean(d2))
+
+    (d1.zip(d2).map{|_d1, _d2 |_d1 * _d2}.sum / d1.size.to_d) - (mean(d1) * mean(d2))
   end
 
   def standard_deviation(d)
-    Math.sqrt(d.map{|i| (i - (mean(d))) ** 2}.sum / d.length)
+    Math.sqrt(d.map{|i| (i - (mean(d))) ** 2}.sum / d.length.to_d)
   end
 
   def correlation_coefficient(d1, d2)
-    covariance(d1, d2) / ( standard_deviation(d1) * standard_deviation(d2) )
+    covariance(d1, d2) / ( standard_deviation(d1) * standard_deviation(d2) ).to_d
+  end
+
+  def autocorrelation_coefficient(d)
+    result = []
+    for lag in 0..d.length - 2
+      result << correlation_coefficient(d[0...(d.length - lag)], d[lag..-1]).round(3).to_f 
+    end
+   
+    result
   end
 end
--- main.rb
+++ main.rb
@@ -1,6 +1,11 @@
 require "./lib/statistics_calculator"
 
 statistics_calculator = StatisticsCalculator.new
+p 'correlation_coefficient'
 data1 = [50,60,70,80,90]
 data2 = [40,70,90,60,100]
 p statistics_calculator.correlation_coefficient(data1, data2)
+
+p 'autocorrelation_coefficient'
+data3 = [1,3,1,3,1,3,1,3,1,3]
+p statistics_calculator.autocorrelation_coefficient(data3)
