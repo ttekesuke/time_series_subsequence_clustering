b05566aab94f805f89fe0bff35358800c1cf01ab
ttekesuke <tekesuke1986@hotmail.com>
2023-10-18 01:54:45 +0900

    chaned decide_gradually process

--- decide_gradually.rb
+++ decide_gradually.rb
@@ -5,17 +5,24 @@ include Utility
 
 class DecideGradually
   def run
-    original = [1,3,5,1,3,5,1,1,1,1,1,3,5,1,1,3,5,1,1,1,1]
-    original_autocorrelation = round_array(compare_original_and_shifted_data(original) {|d1, d2| correlation_coefficient(d1, d2)})
-    result = [1, 3, 5]
-    possible_values = (1..5).to_a   
-    (result.length - 1).upto(original.length - 2) do |counter|
+    possible_range = (1..12)
+    possible_values = possible_range.to_a 
+    original = Array.new(200){ |e| rand(possible_range) }
+    original_autocorrelations_each_length = []
+    for index in 0..original.length
+      p "index:#{index}"
+      original_autocorrelations_each_length << autocorrelation_coefficient(original[0..index])
+    end
+
+    result = original[0..2]
+    result.length.upto(original.length - 1) do |counter|
+      p "counter:#{counter}"
       shortest_euclidean_distance = Float::INFINITY
       optimal_value = nil
+
       possible_values.each do |possible|
-        acf = compare_original_and_shifted_data(result + [possible]) {|d1, d2| correlation_coefficient(d1, d2)}
-        acf.delete_at(-1) 
-        euclidean_distance = euclidean_distance(original_autocorrelation[0..counter], acf)
+        acf = autocorrelation_coefficient(result + [possible])
+        euclidean_distance = euclidean_distance(original_autocorrelations_each_length[counter], acf)
         if shortest_euclidean_distance > euclidean_distance
           shortest_euclidean_distance = euclidean_distance
           optimal_value = possible
@@ -23,11 +30,9 @@ class DecideGradually
       end
       result.push(optimal_value)
     end
-    result_autocorrelation = round_array(compare_original_and_shifted_data(result) {|d1, d2| correlation_coefficient(d1, d2)})
+    result_autocorrelation = autocorrelation_coefficient(result)
     p original
     p result
-    p original_autocorrelation
-    p result_autocorrelation
   end
 end
 
